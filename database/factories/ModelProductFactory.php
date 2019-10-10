78454b847cffe98456e90d3b4303331f84507764
ashraf <ashrafgeneral@hotmail.com>
2019-10-10 17:38:58 +0300

    seeding Data for Product and reviews

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name'=>$faker->word,
+        'detail'=>$faker->paragraph,
+        'price'=>$faker->numberBetween($min = 100, $max = 10000), // 8567,
+        'store'=>$faker->randomDigit,
+        'discount'=>$faker->numberBetween($min = 2, $max = 30),
     ];
 });
--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -4,6 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id'=>function(){
+        return \App\Model\Product::all()->random();
+        },
+        'customer'=>$faker->word,
+        'review'=>$faker->paragraph,
+        'star'=>$faker->numberBetween($min =0, $max = 5), // 8567,
+
     ];
 });
